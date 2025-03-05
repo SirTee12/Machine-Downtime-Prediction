b3303e19f054c78f8e3ee620803660668ed0ab52
SirTee12 <ahmadayantola@gmail.com>
2025-03-06 00:48:55 +0100

    Created a customexception class to print error message

--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,7 @@
 import os
 import logging
 
-def error_message(error, error_detail:sys):
+def error_message_detail(error, error_detail:sys):
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message = "Error occurred in python script name [{}], \
@@ -11,5 +11,11 @@ def error_message(error, error_detail:sys):
                     
     return error_message
 
-    
-    
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, 
+                                                  error_detail=error_detail)
+        
+        def __str__(self):
+            return self.error_message
