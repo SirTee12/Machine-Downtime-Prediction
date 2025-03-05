20b2e88968f7289a1c65585a9e0e8f3928782dc7
SirTee12 <ahmadayantola@gmail.com>
2025-03-05 23:17:56 +0100

    created the logging script

---
+++ src/logging.py
@@ -0,0 +1,14 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+log_path = os.path.join(os.getcwd(), 'log', LOG_FILE)
+os.makedirs(log_path, exist_ok=True)
+
+LOG_FILE_path = os.path.join(log_path, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_path,
+    format="[ %(acstime)s] %(lineno)d %(name)s - %(levelname)s - %(messsage)s"
+)
