24e05b9e059d88d8e6281d6f0f8431da8e79726a
SirTee12 <ahmadayantola@gmail.com>
2025-03-17 19:47:26 +0100

    created the predictpipeline class in predict_pipeline.py

---
+++ src/pipeline/predict_pipeline.py
@@ -0,0 +1,23 @@
+import sys
+import os
+import pandas as pd
+import numpy as pd
+from src.exception import CustomException
+from src.utils import load_object
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+    
+    def predict(self, features):
+        try:
+            model_path = os.path.join('Artifacts', 'model.pkl')
+            preprocessor_path = os.path.join('Artifacts', 'preprocessor.pkl')
+            model = load_object(file_path=model_path)
+            preprocessor = load_object(file_path=preprocessor_path)
+            data_scaled = preprocessor.transform(features)
+            preds = model.predict(data_scaled)
+            return preds
+        except Exception as e:
+            raise CustomException(e, sys)
+        
