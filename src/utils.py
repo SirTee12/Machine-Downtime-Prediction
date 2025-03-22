2a98e0610412a924edda4b0c9b77816a86c1e873
SirTee12 <ahmadayantola@gmail.com>
2025-03-22 23:35:35 +0100

    comment the dll package import

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ import sys
 import pandas as pd
 import numpy as np 
 import pickle
-import dill
+#import dill
 
 from src.logging import logging
 
@@ -21,7 +21,7 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
         
         with open(file_path, 'wb') as file_obj:
-            dill.dump(obj, file_obj)
+            pickle.dump(obj, file_obj)
     except Exception as e:
         raise CustomException(e, sys)
     
